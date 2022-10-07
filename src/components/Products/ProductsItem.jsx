d66d27d47d4f23084fcb15de8f78952e8247d92a
Alexis-Campomanes <alexiscp.developer@gmail.com>
2022-10-07 01:15:14 -0500

    modificación card

--- src/components/Products/ProductsItem.jsx
+++ src/components/Products/ProductsItem.jsx
@@ -1,66 +1,53 @@
 import React from 'react'
-import { FavoriteBorderOutlined, Search, ShoppingCartOutlined } from '@material-ui/icons';
+import { ShoppingCartOutlined } from '@material-ui/icons';
 import { IconButton } from '@material-ui/core';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import './ProductsItem.scss';
+import NotLike from '../../Assets/notlike.png';
+import  Like from '../../Assets/like.png'
+import { useState } from 'react';
 
 
 
 
 const ProductsItem = ({product}) => {
 
+  const [favorite, setFavorite] = useState(true);
+
+  const handleFavorite = () => {
+    setFavorite(!favorite)
+  }
+
+  console.log(favorite)
+
   return (
   
-    <div className='products-card' style={{
-      display:'flex',
-      boxShadow:'5px 10px 18px #888888',
-      borderRadius:'20px',
-      overflow:'hidden',
-      justifyContent:'center',
-      padding:'1rem 0',
-      marginBottom:'2rem',
-      position:'relative'
-    }}>
-      <img src={product.image} alt="imagen" style={{
-        width:'300px'
-      }}/>
+    <div className='products-card'>
+      <div className='p-top'>
+        <img src={product.image} alt="imagen"/>
+            <div className='i-favorite' onClick={handleFavorite}>
+              {
+              <img src={favorite
+                        ?NotLike
+                        :Like
+                        } alt="favorite" />
+              
+              }
+            </div>
+      </div>
             {/* icons */}  
-        <div className='icons' style={{
-          position:'absolute',
-          left: '8px',
-          display:'flex',
-          flexDirection: 'column',
-          gap: '0.8rem'
-        }}>
-          <div>
-            <Link to={`/item/${product.id}`}>
-              <IconButton aria-label="add to shopping cart">
-                <ShoppingCartOutlined style={{
-                    fill:'#034ef7',
+      <div className='i-shopping'>
+              <Link to={`/item/${product.id}`}>
+                <IconButton>
+                  <ShoppingCartOutlined style={{
+                    fill:'#ffffff',
                     cursor:'pointer'
                   }}/>
-              </IconButton>
-            </Link> 
-          </div>
-          <div>
-            <IconButton aria-label="I like it" >
-              <FavoriteBorderOutlined style={{
-                  fill:'#034ef7',
-                  cursor:'pointer',
-                }}/>
-            </IconButton>
-              
-          </div>
-          <div>
-            <IconButton aria-label="Search">
-              <Search style={{
-                  fill:'#034ef7',
-                  cursor:'pointer'
-                }}/>
-            </IconButton> 
-          </div>
+                </IconButton>
+              </Link> 
         </div>     
     </div>
-  
+    
   )
 }
 
